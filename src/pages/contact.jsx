c6e912d8cfffaccf8aa82236cbdbd8953f002c80
Egor <e.galaguz@inbox.ru>
2024-03-29 01:05:54 +0700

    first commit

---
+++ src/pages/contact.jsx
@@ -0,0 +1,11 @@
+function Contact() {
+    return (
+        <div className="contact">
+            <h1>Our contacts</h1>
+            <h6>spa.react.app@gmail.com</h6>
+            <h6>+52 173 52 52</h6>
+        </div>
+    )
+}
+
+export { Contact }
